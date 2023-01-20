b44a4d283462eb1805310bd8e85657262be8e1c0
Jewel Paira <119938577+jewelcssndr@users.noreply.github.com>
2023-01-20 16:05:38 +0800

    Create App.java

---
+++ Exercise3/App.java
@@ -0,0 +1,48 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "T'challa";
+        knight.sayMyName();
+        knight.Attack();
+
+        Character mage = new Character();
+
+        mage.strength = 10;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Jewel";
+        mage.sayMyName();
+        mage.Attack();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Loki";
+        thief.sayMyName();
+        thief.Attack();
+
+        Character ranger = new Character();
+
+        ranger.strength = 10;
+        ranger.agility = 5;
+        ranger.intelligence = 5;
+        ranger.name = "Hawkeye";
+        ranger.sayMyName();
+        ranger.Attack();
+
+        Weapon sword = new Weapon();
+
+        sword.name = "Dagger";
+        sword.damage = 999;
+        sword.rarity = "Legendary";
+        sword.addDamage(1);
+        System.out.println(sword.nameRarity());
+    }
+}
